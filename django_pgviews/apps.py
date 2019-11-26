1157f6267d5726dc8da9b2b890a82efde8932c77
Sarah Betack <sarah@thehelperbees.com>
2019-11-26 09:15:10 -0600

    add setting to allow skipping the automatic sync after migrations

--- django_pgviews/apps.py
+++ django_pgviews/apps.py
@@ -2,6 +2,7 @@ import logging
 
 from django import apps
 from django.db.models import signals
+from django.conf import settings
 
 log = logging.getLogger('django_pgviews.sync_pgviews')
 
@@ -21,12 +22,13 @@ class ViewConfig(apps.AppConfig):
         total = len([a for a in apps.apps.get_app_configs() if a.models_module is not None])
         
         if self.counter == total:
-            log.info('All applications have migrated, time to sync')
             # Import here otherwise Django doesn't start properly
             # (models in app init are not allowed)
             from .models import ViewSyncer
             vs = ViewSyncer()
-            vs.run(force=True, update=True) 
+            if not getattr(settings, 'PGVIEWS_DISABLE_AUTO_SYNC', False):
+                log.info('All applications have migrated, time to sync')
+                vs.run(force=True, update=True) 
 
     def ready(self):
         """Find and setup the apps to set the post_migrate hooks for.
